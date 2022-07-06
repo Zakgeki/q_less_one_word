577d031ae01925fd7e6441168c16d1bea83d1de5
Zach <zachwells@me.com>
2022-07-06 17:14:10 -0400

    moved the gen_perms inside as a closure essentially

--- src/permute.rs
+++ src/permute.rs
@@ -3,7 +3,7 @@ mod tests {
     use super::*;
     #[test]
     fn test_gen_unique_perms() {
-        let result = gen_unique_perms(["a","b","c"].to_vec());
+        let result = permute(["a","b","c"].to_vec());
         let desired = vec![ 
             vec!["a","b","c"], 
             vec!["b","a","c"], 
@@ -18,27 +18,31 @@ mod tests {
 }
 
 /// generates permutations form a given vector
-pub fn gen_unique_perms<T: Clone>( mut arr: Vec<T> ) -> Vec<Vec<T>> {
+pub fn permute<T: Clone>( mut arr: Vec<T> ) -> Vec<Vec<T>> {
+    fn gen_perms<N: Clone>(k: usize, arr: &mut Vec<N>, out: &mut Vec<Vec<N>>) {
+        if k == 0 {
+            out.push( arr.clone() );
+        } else {
+            gen_perms( k - 1, arr , out);
+            
+            for i in 0..k - 1 {
+                if k % 2 == 0 {
+                    arr.swap(i, k - 1);
+                } else {
+                    arr.swap(0, k - 1 );
+                }
+                
+                gen_perms( k - 1, arr, out );
+            }
+        }
+    }
+    
     let mut perms = vec![];
     let k = arr.len();
     gen_perms(k, &mut arr, &mut perms);
+    // for perm in perms {
+    //     println!("{:?}", perm);
+    // }
+    // println!("{:?}", perms);
     perms
-}
-
-fn gen_perms<T: Clone>(k: usize, arr: &mut Vec<T>, out: &mut Vec<Vec<T>>) {
-    if k == 0 {
-        out.push( arr.clone() );
-    } else {
-        gen_perms( k - 1, arr , out);
-        
-        for i in 0..k - 1 {
-            if k % 2 == 0 {
-                arr.swap(i, k - 1);
-            } else {
-                arr.swap(0, k - 1 );
-            }
-            
-            gen_perms( k - 1, arr, out );
-        }
-    }
 }
